c16f6d2afedad774a4d59ba9ea3d7159946b8af6
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-04-17 18:04:52 +0100

    Walls further away are coloured lighter

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const tileSize = 10;
 const colors = {
-  0: "#fff",
-  1: "#000"
+  'player': "#5555AA",
+  0: "#FFFFFF",
+  1: "#333333",
+  2: "#FF0000"
 };
 
 const mapSelect = document.getElementById('map-select');
@@ -202,21 +204,38 @@ function loop(){
         let distance = 0;
     
         while (distance < MAX_DISTANCE) {
-        distance += 0.001;
-        x += dx * 0.001;
-        y += dy * 0.001;
+          distance += 0.001;
+          x += dx * 0.001;
+          y += dy * 0.001;
     
-        // Check if the ray has hit a wall
-        if (scene[Math.floor(y)][Math.floor(x)] > 0) {
-            break;
-        }
+          // Check if the ray has hit a wall
+          if (scene[Math.floor(y)][Math.floor(x)] > 0) {
+              break;
+          }
         }
     
         // Calculate the height of the wall based on its distance from the player
         const wallHeight = STRIPE_HEIGHT / distance;
     
         // Draw the stripe on the screen
-        ctx.fillStyle = "#333";
+        // ctx.fillStyle = "#333";
+
+        hslColor = hexToHsl(colors[scene[Math.floor(y)][Math.floor(x)]])
+        // if(distance>3){
+          // changeBrightness = hslColor[2]*((distance-2)/4)
+          changeBrightness = hslColor[2]*((distance+1)/4)
+          if(changeBrightness>hslColor[2]){
+            hslColor[2] = changeBrightness
+          }
+        // }
+        newColor = hslToHex(hslColor)
+
+        if(newColor.length>7){
+          newColor = "#FFFFFF"
+        }
+
+        ctx.fillStyle = newColor
+        
         ctx.fillRect(i * STRIPE_WIDTH, (CANVAS_HEIGHT - wallHeight) / 2, STRIPE_WIDTH, wallHeight);
     }
 
@@ -230,7 +249,7 @@ function loop(){
     }
 
     // Draw the player on the screen
-    ctx.fillStyle = "#f00";
+    ctx.fillStyle = colors['player'];
     ctx.fillRect(playerX * tileSize - tileSize / 2, playerY * tileSize - tileSize / 2, tileSize, tileSize);
 }
 
@@ -279,6 +298,91 @@ function isBlocking(x, y) {
     return true;
   }
   return false;
+}
+
+function hexToHsl(hex) {
+  // Convert HEX to RGB
+  let r = parseInt(hex.slice(1, 3), 16);
+  let g = parseInt(hex.slice(3, 5), 16);
+  let b = parseInt(hex.slice(5, 7), 16);
+
+  // Convert RGB to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r,g,b);
+  let cmax = Math.max(r,g,b);
+  let delta = cmax - cmin;
+  let h = 0;
+  let s = 0;
+  let l = 0;
+
+  if (delta === 0) {
+    h = 0;
+  } else if (cmax === r) {
+    h = ((g - b) / delta) % 6;
+  } else if (cmax === g) {
+    h = (b - r) / delta + 2;
+  } else {
+    h = (r - g) / delta + 4;
+  }
+
+  h = Math.round(h * 60);
+  if (h < 0) {
+    h += 360;
+  }
+
+  l = (cmax + cmin) / 2;
+
+  if (delta === 0) {
+    s = 0;
+  } else {
+    s = delta / (1 - Math.abs(2 * l - 1));
+  }
+
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return [h,s,l];
+}
+
+function hslToHex(hsl) {
+  let [h, s, l] = hsl;
+  s = s / 100;
+  l = l / 100;
+  let c = (1 - Math.abs(2 * l - 1)) * s;
+  let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+  let m = l - c / 2;
+  let r, g, b;
+  if (h >= 0 && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (h >= 60 && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (h >= 120 && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (h >= 180 && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (h >= 240 && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  r = Math.round((r + m) * 255).toString(16).padStart(2, "0");
+  g = Math.round((g + m) * 255).toString(16).padStart(2, "0");
+  b = Math.round((b + m) * 255).toString(16).padStart(2, "0");
+  return `#${r}${g}${b}`;
 }
 
 setInterval(loop,1000/60)
